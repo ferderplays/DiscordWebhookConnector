f00472a1822b8bc64efb0e7456b3e90581c2aea0
FERDdeveloper <86313430+ferderplays@users.noreply.github.com>
2022-02-23 17:14:40 +0100

    v1.0.0 - i forgot to remove the webhook

--- src/net/ferderplays/dwl/Main.java
+++ src/net/ferderplays/dwl/Main.java
@@ -9,9 +9,8 @@ public class Main {
 		 * or can be made to a bot, that is only for one server and one channel, 
 		 * this could be good for authentication bots */
 		//example:
-		String webhook = "https://discord.com/api/webhooks/945385893362036806/zkFFjCT5tkXiOYT5uff0TkJ7fdLFtz5Ch6-l3XgCXw2ib5UFuHIggKRSkUCw4WziTiX0";
 		try {
-			Sender.sendMessage("f bot", "hi", false, webhook);
+			//here goes the method
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
